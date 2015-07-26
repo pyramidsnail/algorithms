69d4afd736ab778b4bba67375b3a97a05428c102
pyramidsnail <sherry.bim@gmail.com>
2015-07-26 23:47:25 +0200

    modify

--- qs.py
+++ qs.py
@@ -1,16 +1,50 @@
 import sys, os, re
 
-# f = open('QuickSort.txt', 'r')
-f = open('test', 'r')
-
+f = open('QuickSort.txt', 'r')
+# f = open('test', 'r')
 lines = f.readlines()
 lst = []
 for i in lines:
     lst.append(int(i.strip()))
 
-
+total = 0
 def partition(arr):
+    global total
+    total += len(arr)-1
+    ### choose the first as the pivot
     p = arr[0]
+
+
+    # ####choose the last as the pivot
+    # p = arr[len(arr)-1]
+    # arr[len(arr)-1] = arr[0]
+    # arr[0] = p
+
+
+    # ### choose median
+    # first = arr[0]
+    # last = arr[len(arr)-1]
+    # if len(arr)%2==0:
+    #     middle = arr[len(arr)/2-1]
+    #     index = len(arr)/2-1
+    # else:
+    #     middle = arr[int(len(arr)/2)]
+    #     index = int(len(arr)/2)
+    # if first == sorted([first, middle, last])[1]:
+    #     p = first
+    #     index = 0
+    # if last == sorted([first, middle, last])[1]:
+    #     p = last
+    #     index = len(arr)-1
+    # if middle == sorted([first, middle, last])[1]:
+    #     p = middle
+    
+    # arr[index] = arr[0]
+    # arr[0] = p
+
+
+
+    
     i = 1
     for j in xrange(i, len(arr)):
         if arr[j]<p:
@@ -24,19 +58,24 @@ def partition(arr):
     return i-1
 
     
-def qs(arr, total):
+def qs(arr):
+    global total
     if len(arr) == 1:
-        return total
+        return
     else:
         # p = arr[0]
         i = partition(arr)
-        total += len(arr)
-        qs(arr[0:i+1], total)
-        qs(arr[i+1:len(arr)], total)
+        if i>0:
+            qs(arr[0:i])
+            # total += len(arr[0:i+1])-1
+        if len(arr)>i+1:
+            qs(arr[i+1:len(arr)])
+            # total += len(arr[i+1:len(arr)])-1
 
 
-total = 0
-print qs(lst, total)
+
+qs(lst)
+print total
 
 
 # total = 0
